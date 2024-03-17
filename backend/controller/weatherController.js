f0e48751748d77cac4627968dddfd24ec45f522b
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-17 20:23:25 +0530

    API Level 1 is ON

--- backend/controller/weatherController.js
+++ backend/controller/weatherController.js
@@ -1,26 +1,23 @@
 const axios = require("axios");
-// const getWeather = async () => {
-
-// }
+const cities = {
+  locations: [
+    {
+      q: "London",
+    },
+    {
+      q: "Mumbai",
+    },
+    {
+      q: "Bangalore",
+    },
+    {
+      q: "Paris",
+    },
+  ],
+};
 
 const weatherController = async (req, res) => {
   try {
-    const cities = {
-      locations: [
-        {
-          q: "London",
-        },
-        {
-          q: "Mumbai",
-        },
-        {
-          q: "Bangalore",
-        },
-        {
-          q: "Paris",
-        },
-      ],
-    };
     const { data } = await axios.post(
       `http://api.weatherapi.com/v1/current.json?key=${process.env.VITE_WEATHER_API}&q=bulk`,
       cities
@@ -59,4 +56,43 @@ const weatherController = async (req, res) => {
   }
 };
 
-module.exports = weatherController;
+const cityController = async (req, res) => {
+  try {
+    const { data } = await axios.post(
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.VITE_WEATHER_API}&q=bulk`,
+      cities
+    );
+    const id = req.params.id;
+    if (data && id<4) {
+      const cityWeather = data.bulk;
+      const CityDetail = {
+        name: cityWeather[id].query.location.name,
+        country: cityWeather[id].query.location.country,
+        temperature: cityWeather[id].query.current.temp_c,
+        humidity: cityWeather[id].query.current.humidity,
+        condition: cityWeather[id].query.current.condition.text,
+        icon_url: cityWeather[id].query.current.condition.icon,
+      };
+      res.status(200).send({
+        success: true,
+        message: "Got City Details",
+        CityDetail,
+      });
+    } else {
+      res.status(404).send({
+        success: true,
+        message: "City Not Found",
+        cityWeather,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = { weatherController, cityController };
--- backend/routes/weatherRoute.js
+++ backend/routes/weatherRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const weatherController = require('../controller/weatherController')
+const {weatherController,cityController} = require('../controller/weatherController')
 
 const router = express.Router()
 
 router.get('/',weatherController)
+router.get('/:id',cityController)
 
 module.exports = router
