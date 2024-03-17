4f8910266e0237e759741cf2d9c1056a3d00a9a3
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-17 11:00:55 +0530

    Frontend Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,73 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Components/Card";
 
+import axios from "axios";
+
 function App() {
+  const [weather, setWeather] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const cities = {
+    locations: [
+      {
+        q: "London",
+      },
+      {
+        q: "Mumbai",
+      },
+      {
+        q: "Bangalore",
+      },
+      {
+        q: "Paris",
+      },
+    ],
+  };
+
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        setLoading(true);
+        const {data} = await axios.post(
+          `http://api.weatherapi.com/v1/current.json?key=${
+            import.meta.env.VITE_WEATHER_API
+          }&q=bulk`,
+          cities
+        );
+        setWeather(data.bulk);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    getWeather();
+    //eslint-disable-next-line
+  }, []);
   return (
     <>
       <div className="h-32">
-        <p className="text-5xl font-bold from-stone-900" style={{fontFamily:"SAMAN"}}>Weather App</p>
+        <p
+          className="text-5xl font-bold from-stone-900 gradient-text"
+          style={{ fontFamily: "SAMAN",color:"#D9FFF8" }}
+        >
+          Weather App
+        </p>
       </div>
       {/* grid container */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-10 gap-y-20 h-fit text-xl">
-        {/* product cards */}
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {loading ? (
+        "Loading ..."
+      ) : (
+        <>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-10 gap-y-20 h-fit text-xl">
+            {/* product cards */}
+            {weather.map((city,index)=>
+              (<Card key={index} city={city} />)
+            )
+            }
+          </div>
+        </>
+      )}
     </>
   );
 }
--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Card = () => {
+const Card = ({ city }) => {
+  const [img, setImg] = useState([]);
+  useEffect(() => {
+    const getImg = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.unsplash.com/search/photos?page=1&per_page=1&query=${
+            city?.query.q
+          }&client_id=${import.meta.env.VITE_CLIENT_ID}`
+        );
+        console.log(data);
+        setImg(data?.results[0].urls?.regular);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getImg();
+  }, []);
   return (
     <>
       <a
-        href="https://www.weatherapi.com/"
+        href=""
         className="flex flex-col bg-white drop-shadow hover:drop-shadow-lg hover:opacity-70 rounded-md"
       >
-        <img
-          src=" https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png"
+        <div
           alt="Fiction Product"
-          className="h-40 object-cover rounded-tl-md rounded-tr-md"
-        />
-        <div className="px-3 py-2">
-          <h1 className="font-semibold">Product One</h1>
-          <p className="text-sm">$69.69</p>
+          className="h-36 flex object-cover rounded-tl-md rounded-tr-md"
+          style={{
+            backgroundImage: `url(${img})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            position: "relative",
+          }}
+        >
+          <div className="background-overlay"></div>
+          <img
+            src={`https:${city?.query.current.condition.icon}`}
+            alt="Weather"
+            className="h-16 w-16 m-14 mx-30 object-cover rounded-tl-md rounded-tr-md"
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0.0);",
+              zIndex: "2",
+              border: "1px solid rgba(255, 255, 255, 0.2)",
+              borderRadius: "10px",
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+            }}
+          />
+          <p className="lg:text-4xl text-5xl font-sans font-semibold text-gray-100 flex items-center m-20">
+            {Math.round(city?.query.current.temp_c)}{" "}
+            <sup className="lg:text-2xl text-2xl">o </sup>
+            <span className="lg:text-4xl text-4xl"> C</span>
+          </p>
+        </div>
+        <div className="inline">
+          {city?.query.current.condition.text}
+          
+        </div>
+        <div className="px-3 pb-2">
+          <h1 className=" text-2xl font-semibold">{city?.query.q},</h1>
+          <p className="text-sm">{city?.query.location.country}</p>
         </div>
       </a>
     </>
